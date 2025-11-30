0f5184695d635c701daa01110f81a9e0513c5f70
samksr <127689491+samksr@users.noreply.github.com>
2025-12-01 01:37:48 +0530

    Delete pm2/ecosystem.config.js

--- pm2/ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apps: [{
-    name: "solana-hunter-bot",
-    script: "index.js",
-    watch: false,
-    autorestart: true,
-    max_memory_restart: '450M' // Safety cap
-  }]
-};
